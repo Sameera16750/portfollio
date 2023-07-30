0f7e540db9408f4bb7eb9fbde3baa66f46d23ade
Samiya2000 <Sameera@technography.ca>
2023-07-30 18:43:03 +0530

    card-animation js added

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
 
+  ngOnInit() {
+    window.scroll(0,1 )
+  }
 }
